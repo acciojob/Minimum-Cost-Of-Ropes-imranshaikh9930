bdc16381757c541e12e44c12f616d5af3cc2f6fe
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-12 22:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function calculateMinCost() {
+function calculateMinCost(ropes) {
   //your code here
+	ropes = ropes.map(String);
+
+    while (ropes.length > 1) {
+        // Find the two shortest ropes in the array.
+        ropes.sort((a, b) => a.length - b.length);
+
+        // Combine the two shortest ropes.
+        const combinedRope = ropes[0] + ropes[1];
+
+        // Remove the two shortest ropes and add the combined rope.
+        ropes.splice(0, 2, combinedRope);
+    }
+
+    // The last rope in the array is the minimum cost.
+    return parseInt(ropes[0]);
   
   
   
